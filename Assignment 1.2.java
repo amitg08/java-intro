b4f643b44745b5e3837c8c4176f23d8de761925d
amitg08 <a4amit92@gmail.com>
2017-03-07 03:05:50 +0530

    Add files via upload

---
+++ Assignment 1.2.java
@@ -0,0 +1,17 @@
+
+public class Variables {
+	public static void main(String[] args){
+		int a=20 , b=10;
+		b= a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		int e=a&b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+
+	}
+
+}
